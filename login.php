d082a10b5243e9ff3a9193d28344bdf2bcf5eabc
Monday_David_Sarka <david_md@hamari.agency>
2023-01-05 13:23:26 +0100

    Encrypt password

--- login.php
+++ login.php
@@ -11,16 +11,19 @@
     <div>
         <?php
             if(isset($_POST["submit"])) {
-                echo "Welcome to Insme sun Chi Kudi Dashboard!";
                 $fullName = $_POST["fullname"];
                 $email = $_POST["email"];
                 $password = $_POST["password"];
                 $passwordRepeat = $_POST["repeat-password"];
 
+                $passwordHash = password_hash($password, PASSWORD_DEFAULT);
                 $errors = array();
                 
-                if(empty($fullname) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                if(empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
                     array_push($errors, "All Fields are required");
+                }else {
+                    echo "Welcome to your dashboard $fullName <br>";
+                    echo "Your email is: $email";
                 }
 
                 if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -35,16 +38,15 @@
                     array_push($errors, "Password must match bro!");
                 }
 
-                if (count($errors) > 0) {
+                if (count($errors)>0) {
                     foreach ($errors as $error) {
-                        echo "<div class='container'>$error</div>";
+                        echo "<div class=''>$error</div>";
                     }
                 }else {
 
                 }
             }
         ?>
-    </div>
-    
+    </div>    
 </body>
 </html>
